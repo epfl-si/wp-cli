008f2178443ec1c8eba3858e44879e1ee9521c5c
LuluTchab <l.chaboudez@gmail.com>
2019-05-29 13:06:11 +0200

    Update function header to correctly display help in WPCLI

--- src/EPFL_Theme_Command.php
+++ src/EPFL_Theme_Command.php
@@ -14,7 +14,43 @@ class EPFL_Theme_Command extends \Theme_Command  {
 
     var $THEME_FOLDER = "/wp/wp-content/themes/";
 
-    /* Override existing install function*/
+   /**
+	 * Installs one or more themes.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <theme|zip|url>...
+	 * : One or more themes to install. Accepts a theme slug, the path to a local zip file, or a URL to a remote zip file.
+	 *
+	 * [--version=<version>]
+	 * : If set, get that particular version from wordpress.org, instead of the
+	 * stable version.
+	 *
+	 * [--force]
+	 * : If set, the command will overwrite any installed version of the theme, without prompting
+	 * for confirmation.
+	 *
+	 * [--activate]
+	 * : If set, the theme will be activated immediately after install.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Install the latest version from wordpress.org and activate
+	 *     $ wp theme install twentysixteen --activate
+	 *     Installing Twenty Sixteen (1.2)
+	 *     Downloading install package from http://downloads.wordpress.org/theme/twentysixteen.1.2.zip...
+	 *     Unpacking the package...
+	 *     Installing the theme...
+	 *     Theme installed successfully.
+	 *     Activating 'twentysixteen'...
+	 *     Success: Switched to 'Twenty Sixteen' theme.
+	 *
+	 *     # Install from a local zip file
+	 *     $ wp theme install ../my-theme.zip
+	 *
+	 *     # Install from a remote zip file
+	 *     $ wp theme install http://s3.amazonaws.com/bucketname/my-theme.zip?AWSAccessKeyId=123&Expires=456&Signature=abcdef
+	 */
     public function install( $args, $assoc_args ) {
 
         /* Looping through themes to install */
@@ -79,7 +115,30 @@ class EPFL_Theme_Command extends \Theme_Command  {
 
 
 
-    /* Override existing delete function*/
+    /**
+	 * Deletes one or more themes.
+	 *
+	 * Removes the theme or themes from the filesystem.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [<theme>...]
+	 * : One or more themes to delete.
+	 *
+	 * [--all]
+	 * : If set, all themes will be deleted except active theme.
+	 *
+	 * [--force]
+	 * : To delete active theme use this.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     $ wp theme delete twentytwelve
+	 *     Deleted 'twentytwelve' theme.
+	 *     Success: Deleted 1 of 1 themes.
+	 *
+	 * @alias uninstall
+	 */
     public function delete( $args )
     {
         /* Looping through themes to install */
